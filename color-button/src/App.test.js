001761e665d4236699e3c2548664653a5879bad4
yyogue <youssouf.e.yogue@aexp.com>
2023-10-31 15:02:22 -0400

    logRoles

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import {  render, screen } from '@testing-library/react';
+import { logRoles } from '@testing-library/react';
 import App from './App';
 
+
 test('button has correct initial color', () => {
-  render(<App />)
+  const { container } = render(<App />)
+  logRoles(container)
 
   //find an element with a role of button and text of 'Change to blue'
   const colorButton = screen.getByRole('button', { name: 'Change to blue'})
