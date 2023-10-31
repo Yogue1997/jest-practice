eb2d8cfdeb484d9ae0b8b27d3922935b295ec1e3
yyogue <youssouf.e.yogue@aexp.com>
2023-10-31 15:29:37 -0400

    onClick event passed

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,20 +1,27 @@
-import {  render, screen } from '@testing-library/react';
-import { logRoles } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-
-test('button has correct initial color', () => {
-  const { container } = render(<App />)
-  logRoles(container)
+test('button has correct initial color, and update when clicked', () => {
+  render(<App />);
 
   //find an element with a role of button and text of 'Change to blue'
-  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
   //expect background to be red
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  //click button
+  fireEvent.click(colorButton);
+
+  //expect background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  //expect background color to be blue
+  expect(colorButton).toHaveTextContent('Change to red')
 });
 
 
-test('button turns blue when clicked', () => {
-
-});
+// test('button turns blue when clicked', () => {
+//   render(<App />)
+//   const colorButton = screen.getByRole('button', { name: 'change to blue' })
+// });
